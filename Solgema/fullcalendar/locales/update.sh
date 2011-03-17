4a6962018a7baa7a27282f0f89ba0885227bc191
thomasdesvenain <thomasdesvenain@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2011-03-17 11:07:12 +0000

    calendar display layout are internationalized is french translated.

--- Solgema/fullcalendar/locales/update.sh
+++ Solgema/fullcalendar/locales/update.sh
@@ -1,3 +1,6 @@
 domain=Solgema.fullcalendar
 i18ndude rebuild-pot --pot $domain.pot --create $domain ../
 i18ndude sync --pot $domain.pot */LC_MESSAGES/$domain.po
+
+i18ndude rebuild-pot --pot plone.pot --merge plone-manual.pot --create plone ../profiles
+i18ndude sync --pot plone.pot */*/plone.po
