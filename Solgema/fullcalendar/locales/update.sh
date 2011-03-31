7e51753ba3ddf0ba1d115d1e1b000b255e41ce76
thomasdesvenain <thomasdesvenain@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2011-03-31 13:25:12 +0000

    fixed plone domain use for plone 3

--- Solgema/fullcalendar/locales/update.sh
+++ Solgema/fullcalendar/locales/update.sh
@@ -3,4 +3,4 @@ i18ndude rebuild-pot --pot $domain.pot --create $domain ../
 i18ndude sync --pot $domain.pot */LC_MESSAGES/$domain.po
 
 i18ndude rebuild-pot --pot plone.pot --merge plone-manual.pot --create plone ../profiles
-i18ndude sync --pot plone.pot */*/plone.po
+i18ndude sync --pot plone.pot ../i18n/Solgema-plone-*.po
