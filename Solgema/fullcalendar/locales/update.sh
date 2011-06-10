c6fab66f54d901457287b1d6a6a789953d63f36c
vincentfretin <vincentfretin@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2011-06-10 10:38:31 +0000

    Don't put plone domain in locales for backward compatibility with Plone 3.3

--- Solgema/fullcalendar/locales/update.sh
+++ Solgema/fullcalendar/locales/update.sh
@@ -2,6 +2,5 @@ domain=Solgema.fullcalendar
 i18ndude rebuild-pot --pot $domain.pot --create $domain ../
 i18ndude sync --pot $domain.pot */LC_MESSAGES/$domain.po
 
-i18ndude rebuild-pot --pot plone.pot --merge plone-manual.pot --create plone ../profiles
+#i18ndude rebuild-pot --pot plone.pot --merge plone-manual.pot --create plone ../profiles
 i18ndude sync --pot plone-manual.pot ../i18n/Solgema-plone-*.po
-i18ndude sync --pot plone-manual.pot */LC_MESSAGES/plone.po
