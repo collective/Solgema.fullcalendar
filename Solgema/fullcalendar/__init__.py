c73a2ec5d4cffa58afbb7daf9c547ca2af6b641a
tdesvenain <thomas.desvenain@gmail.com>
2012-04-13 17:48:13 +0200

    removed initialize

--- Solgema/fullcalendar/__init__.py
+++ Solgema/fullcalendar/__init__.py
@@ -1,7 +1,3 @@
 import logging
 
 log = logging.getLogger('Solgema.fullcalendar')
-
-def initialize(context):
-    """Initializer called when used as a Zope 2 product."""
-    # Initialize content types
