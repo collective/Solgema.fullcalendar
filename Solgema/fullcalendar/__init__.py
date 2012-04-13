4fefb2efe5a3bc435aa955df5afe9a938fbbf6d8
Tom Gross <itconsense@gmail.com>
2012-04-13 17:19:26 +0200

    fixed MANIFEST.in and removed unused initialize

--- Solgema/fullcalendar/__init__.py
+++ Solgema/fullcalendar/__init__.py
@@ -2,6 +2,3 @@ import logging
 
 log = logging.getLogger('Solgema.fullcalendar')
 
-def initialize(context):
-    """Initializer called when used as a Zope 2 product."""
-    # Initialize content types
