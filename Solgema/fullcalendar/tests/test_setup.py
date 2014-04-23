72ba51a43b00cb482ecbe303cfff01a1e697429f
Laurent Lasudry <lasudry@gmail.com>
2014-04-23 13:12:22 +0200

    Add test for issue #31

--- Solgema/fullcalendar/tests/test_setup.py
+++ Solgema/fullcalendar/tests/test_setup.py
@@ -1,9 +1,15 @@
 # -*- coding: utf-8 -*-
 
+from zope.interface import directlyProvides
+from zope.component import queryMultiAdapter
+from plone.app.testing.helpers import login
 from plone.app.testing import setRoles
 from plone.app.testing import TEST_USER_ID
+from plone.app.testing import TEST_USER_NAME
 from plone.browserlayer.utils import registered_layers
+
 from Solgema.fullcalendar.testing import INTEGRATION_TESTING
+from Solgema.fullcalendar.interfaces import ISolgemaFullcalendarLayer
 
 import unittest2 as unittest
 
@@ -16,7 +22,9 @@ class InstallTestCase(unittest.TestCase):
 
     def setUp(self):
         self.portal = self.layer['portal']
+        self.request = self.layer['request']
         self.skins = self.portal['portal_skins']
+        setRoles(self.portal, TEST_USER_ID, ['Manager'])
 
     def test_installed(self):
         qi = self.portal['portal_quickinstaller']
@@ -29,6 +37,15 @@ class InstallTestCase(unittest.TestCase):
     def test_skin_layers(self):
         self.assertIn('solgemafullcalendar', self.skins)
 
+    def test_view_available(self):
+        directlyProvides(self.request, ISolgemaFullcalendarLayer)
+        login(self.portal, TEST_USER_NAME)
+        self.portal.invokeFactory('Folder', 'folder')
+        folder = getattr(self.portal, 'folder')
+        view = queryMultiAdapter((folder, self.request),
+                                 name='solgemafullcalendar_view')
+        self.assertTrue(view is not None)
+
 
 class UninstallTestCase(unittest.TestCase):
 
@@ -36,6 +53,7 @@ class UninstallTestCase(unittest.TestCase):
 
     def setUp(self):
         self.portal = self.layer['portal']
+        self.request = self.layer['request']
         self.skins = self.portal['portal_skins']
         setRoles(self.portal, TEST_USER_ID, ['Manager'])
         self.qi = self.portal['portal_quickinstaller']
@@ -50,3 +68,11 @@ class UninstallTestCase(unittest.TestCase):
 
     def test_skin_layers_removed(self):
         self.assertNotIn('solgemafullcalendar', self.skins)
+
+    def test_view_unavailable(self):
+        login(self.portal, TEST_USER_NAME)
+        self.portal.invokeFactory('Folder', 'folder')
+        folder = getattr(self.portal, 'folder')
+        view = queryMultiAdapter((folder, self.request),
+                                 name='solgemafullcalendar_view')
+        self.assertTrue(view is None)
