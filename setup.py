f72d1a5bf152205050aaee5f7798cd9071702146
Benoît Suttor <bsuttor@imio.be>
2016-04-05 14:29:05 +0200

    Back to development: 2.3.7

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import find_packages
 from setuptools import setup
 
-version = '2.3.6'
+version = '2.3.7.dev0'
 long_description = (
     open('README.rst').read() + '\n' +
     open('CHANGES.rst').read()
