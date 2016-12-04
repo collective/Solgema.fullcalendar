8d206ae3098c72a6880be059fb5f83522ccd7180
Thiago Curvelo <tcurvelo@gmail.com>
2016-12-04 20:17:02 -0300

    Set hour format for pt-br

--- Solgema/fullcalendar/browser/views.py
+++ Solgema/fullcalendar/browser/views.py
@@ -332,7 +332,7 @@ class SolgemaFullcalendarEventJS(BrowserView):
             return '{month: \'MMMM yyyy\', week: "MMM d[ yyyy]{ \'-\'[ MMM] d yyyy}", day: \'dddd, MMM d, yyyy\'}'
 
     def getHourFormat(self):
-        if self.portal_language in ['fr', 'de', 'it', 'nl']:
+        if self.portal_language in ['fr', 'de', 'it', 'nl', 'pt-br']:
             return 'HH:mm'
         else:
             return 'h(:mm)tt'
