92659ea4f1d9c57809d55a4482a1249ef9d2960e
hvelarde <hector.velarde@gmail.com>
2016-12-05 23:11:50 -0200

    Skip incorrect test

--- Solgema/fullcalendar/tests/test_setup.py
+++ Solgema/fullcalendar/tests/test_setup.py
@@ -69,6 +69,11 @@ class UninstallTestCase(unittest.TestCase):
     def test_skin_layers_removed(self):
         self.assertNotIn('solgemafullcalendar', self.skins)
 
+    # FIXME: this test (and the corresponding in the InstallTestCase)
+    #        is wrong; it should check if `solgemafullcalendar_view`
+    #        has been removed from FTI `view_methods` attribute
+    #        view is registered in ZCML and will be always available
+    @unittest.expectedFailure
     def test_view_unavailable(self):
         login(self.portal, TEST_USER_NAME)
         self.portal.invokeFactory('Folder', 'folder')
