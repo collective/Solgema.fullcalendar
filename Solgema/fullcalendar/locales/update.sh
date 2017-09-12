e9cbd11a3f07d23e0d48d0a7c6ada2372114d7a7
hvelarde <hector.velarde@gmail.com>
2017-09-12 15:08:14 -0300

    Update i18n

--- Solgema/fullcalendar/locales/update.sh
+++
@@ -1,5 +0,0 @@
-domain=Solgema.fullcalendar
-i18ndude rebuild-pot --pot $domain.pot --create $domain ../
-i18ndude sync --pot $domain.pot */LC_MESSAGES/$domain.po
-
-i18ndude sync --pot plone-manual.pot */LC_MESSAGES/plone.po
